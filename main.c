479af77108581cef0bfb141650459ddedce0cd8c
ahmedmamdouh32 <ahmedmamdouh.am32@gmail.com>
2023-12-09 17:11:06 +0200

    Latest Version

---
+++ main.c
@@ -0,0 +1,48 @@
+
+#include "HAL/LM35.h"
+#include "MCAL/ADC.h"
+#include "MCAL/DIO.h"
+#include "HAL/LCD.h"
+
+
+void Display_Temperature();
+
+
+int main(void)
+{
+	
+	
+	PinMode(DIO_PD4,OUTPUT);
+	PinMode(DIO_PD5,OUTPUT);
+	PinMode(DIO_PD6,OUTPUT);
+	PinMode(DIO_PD7,OUTPUT);
+	PinMode(DIO_PB0,OUTPUT);
+	PinMode(DIO_PB1,OUTPUT);
+	//PinMode(DIO_PC0,INPUT);
+	
+	
+	LCD_init();
+	ADC_init(ADC_PRESCALAR_128,AVCC);
+	
+	
+	
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+Display_Temperature();	
+    }
+}
+
+
+
+void Display_Temperature(){
+	LCD_SetCursor(0,0);
+	LCD_WriteString("Temperature is :");
+	LCD_SetCursor(1,6);
+	LCD_WriteNumber(temp_read(ADC_CHANNEL_0));
+	LCD_WriteString(" C ");
+}
+
+
---
+++ types.h
@@ -0,0 +1,21 @@
+/*
+ * types.h
+ *
+ * Created: 7/19/2023 12:33:22 AM
+ *  Author: Ahmed Mamdouh
+ */ 
+
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+
+typedef unsigned char uint8;
+typedef unsigned int uint16;
+typedef unsigned long uint32;
+typedef char int8;
+typedef int int16;
+typedef long int32;
+
+
+#endif /* TYPES_H_ */
